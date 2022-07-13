c57eef7aa0129715e3c7a64d665d34b5490ad3e7
Priyaz1212 <haripriya.talala@gmail.com>
2022-07-13 14:20:44 +0800

    Edit n Del TM

--- FirstTest/Program.cs
+++ FirstTest/Program.cs
@@ -35,3 +35,100 @@ else
     Console.WriteLine("Login failed, Test failed"); 
 }
 
+//--------------07072022---------------------
+//Navigate to Administration > Time and Material
+IWebElement adminDropDown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+adminDropDown.Click();
+
+//Open Time & Materials
+IWebElement tmDropDown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmDropDown.Click();
+
+
+//Click Create New
+IWebElement createNewTxtBox = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+createNewTxtBox.Click();
+
+//input Code TextBox
+IWebElement codeTxtBox = driver.FindElement(By.Id("Code"));
+codeTxtBox.SendKeys("Test12");
+
+//input Code TextBox
+IWebElement descTxtBox = driver.FindElement(By.Id("Description"));
+descTxtBox.SendKeys("Test12Desc");
+
+//input Code TextBox
+IWebElement priceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+priceTag.Click();
+
+// input price
+IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+priceTextbox.SendKeys("6456");
+
+// click on save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(3000);
+
+// go to the last page
+IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+goToLastPageButton.Click();
+
+
+// check if material record has been created successfully
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if (newCode.Text == "Test12")
+{
+    Console.WriteLine("New record created successfully.");
+}
+else
+{
+    Console.WriteLine("New Record not created");
+}
+///Edit Functionality
+///
+// go to the last page
+IWebElement goToLastPageButton1 = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+goToLastPageButton1.Click();
+
+IWebElement lastRecEdit = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[5]/a[1]")); 
+lastRecEdit.Click();
+
+//input Code TextBox
+IWebElement codeTxtBox1 = driver.FindElement(By.Id("Code"));
+codeTxtBox1.Clear();
+codeTxtBox1.SendKeys("Test12New");
+
+//input Code TextBox
+IWebElement descTxtBox1 = driver.FindElement(By.Id("Description"));
+descTxtBox1.Clear();
+descTxtBox1.SendKeys("Test12DescNew");
+
+//input Code TextBox
+IWebElement priceTag1 = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+priceTag1.Click();
+
+// input price
+IWebElement priceTextbox1 = driver.FindElement(By.Id("Price"));
+priceTextbox1.Clear();
+priceTag1.Click();
+priceTextbox1.SendKeys("645653");
+
+// click on save button
+IWebElement saveButton1 = driver.FindElement(By.Id("SaveButton"));
+saveButton1.Click();
+Thread.Sleep(3000);
+
+//Delete
+//Go to Last Page
+// go to the last page
+IWebElement goToLastPageButton3 = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+goToLastPageButton3.Click();
+
+IWebElement deletRecLast = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+deletRecLast.Click();
+
+driver.SwitchTo().Alert().Accept();
+
+
